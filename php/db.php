89a9104d49d8afa6830bb068012db38fc19eeec1
PEzwartsEDU <pe.zwarts@student.alfa-college.nl>
2025-12-10 13:10:11 +0100

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,15 +1,16 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "hondenshopnl";
+    $servername = "localhost";
+    $username   = "root";
+    $password   = "";
+    $dbname     = "hondenshopnl";
 
-try {
-    $conn = new PDO(
-        "mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password
-    );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $m) {
-    echo "Verbinding mislukt: " . $m->getMessage();
-    exit();
-}
+    try {
+        $conn = new PDO(
+            "mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password
+        );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $m) {
+        echo "Verbinding mislukt: " . $m->getMessage();
+        exit();
+    }
+?>
